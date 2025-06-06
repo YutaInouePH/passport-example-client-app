06401957da269ed35c157addc9bcd9d09f9c27f6
Yuta Inoue <58018461+YutaInouePH@users.noreply.github.com>
2025-06-06 14:45:16 +0900

    implement oauth 2 flow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@sidebase/nuxt-auth']
+  modules: [
+    '@bitinflow/nuxt-oauth',
+  ],
+
+  ssr: false,
+
+  oauth: {
+    redirect: {
+      home: '/home',
+    },
+    endpoints: {
+      authorization: 'http://localhost:8002/oauth/authorize',
+      token: 'http://localhost:8002/oauth/token',
+      userInfo: 'http://localhost:8002/api/users/me',
+      logout: 'http://localhost:8002/oauth/logout'
+    },
+    clientId: '019743a8-72be-7116-a0f3-2d0732d2984f',
+    scope: ['']
+  },
 })
---
+++ pages/home.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {useAuth, useNuxtApp} from "#imports";
+
+const {user, signOut} = await useAuth();
+
+definePageMeta({
+  middleware: ["auth"]
+})
+
+const { $api } = useNuxtApp()
+
+$api.get('user')
+    .then((response: any) => {
+      console.log(response.data)
+    })
+    .catch((error: any) => {
+      console.log(error)
+    })
+</script>
+
+<template>
+  <div v-if="user">
+    <p>Hello {{ user.name }}</p>
+    <p>Your email is {{ user.email }}</p>
+
+    <button @click="signOut">
+      Sign Out
+    </button>
+  </div>
+</template>
